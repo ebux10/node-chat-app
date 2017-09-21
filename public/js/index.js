6a6b8c9cb82c66945d7f687cf2c7eaa11caf7567
Chukwuebuka <ebeluolisa@gail.com>
2017-09-21 02:49:14 -0400

    Added timestamps using moment

--- public/js/index.js
+++ public/js/index.js
@@ -9,26 +9,30 @@ socket.on("disconnect" , function(){
 });
 
 socket.on("newMessage" , function(message){
+  var formattedTime = moment(message.createdAt).format("h:mm a");
   console.log("New Message" , message);
   var li = jQuery("<li></li>");
-  li.text(`${message.from} : ${message.text}`);
+  li.text(`${message.from} ${formattedTime} : ${message.text}`);
   jQuery("#messages").append(li);
 });
 
 socket.on("newLocationMessage" , function(message){
+  var formattedTime = moment(message.createdAt).format("h:mm a");
   var li = jQuery("<li></li>");
   var a = jQuery("<a target='_blank'>My Current Location</a>");
-  li.text(`${message.from} : `);
+  li.text(`${message.from} ${formattedTime} : `);
   a.attr('href',message.url);
   li.append(a);
   jQuery("#messages").append(li);
 });
 
+var messageTextbox = jQuery("[name=message]");
+
 jQuery("#message-form").on("submit" , function(e){
   e.preventDefault();
   
-  socket.emit("createMessage" , {from : "User", text : jQuery("[name=message]").val()},() =>{
-    
+  socket.emit("createMessage" , {from : "User", text : messageTextbox.val()},function(){
+    messageTextbox.val("");
   });
 });
 
@@ -37,12 +41,15 @@ locationButton.on("click",function(){
   if(!navigator.geolocation){
     return alert("Geolocation is not supported by your browser");
   }
+  locationButton.attr("disabled" , "disabled").text("Sending Location...");
   navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr("disabled").text("Send Location");
     socket.emit("createLocationMessage" , {
       latitude : position.coords.latitude,
       longitude : position.coords.longitude
     });
 } , function(){
+    locationButton.removeAttr("disabled").text("Send Location");
   alert("Unable to fetch Location");
   });
 });
